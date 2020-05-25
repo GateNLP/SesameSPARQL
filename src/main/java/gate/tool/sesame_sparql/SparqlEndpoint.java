aee7ae2658aab3a227e442ee4afe31e3c18b383c
Johann Petrak <johann.petrak@gmail.com>
2020-05-25 12:20:01 +0200

    Use slightly more recent sesame versin. Note: later versions do not work with the server URL any more and need the Sesame repo URL.

--- src/main/java/gate/tool/sesame_sparql/SparqlEndpoint.java
+++ src/main/java/gate/tool/sesame_sparql/SparqlEndpoint.java
@@ -52,6 +52,7 @@ import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResult;
 import org.openrdf.query.TupleQueryResultHandler;
 import org.openrdf.query.TupleQueryResultHandlerException;
+import org.openrdf.query.QueryResultHandlerException;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
@@ -403,7 +404,14 @@ public class SparqlEndpoint {
     public void setPrintHeaders(boolean v) {
       printHeaders = v;
     }
-    
+   
+    @Override
+    public void handleLinks(List<String> linkUrls) throws QueryResultHandlerException {
+    }
+
+    @Override 
+    public void handleBoolean(boolean value) throws QueryResultHandlerException {
+    } 
     
     @Override
     public void handleSolution(BindingSet bs)
